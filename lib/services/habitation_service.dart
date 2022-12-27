b319798b887818c12d49449028d49fb691317927
Louis Alary <louis.alary34@gmail.com>
2022-12-27 10:40:57 +0100

    Render correct habitat type

--- lib/services/habitation_service.dart
+++ lib/services/habitation_service.dart
@@ -34,7 +34,7 @@ class HabitationService{
   }
 
   List<Habitation> getAppartements(){
-    return _getHabitations(isHouse: true);
+    return _getHabitations(isHouse: false);
   }
 
   List<Habitation> _getHabitations({bool isHouse = true}){
